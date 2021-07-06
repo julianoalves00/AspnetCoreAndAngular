3d2149c497bb9f7ef2306e8f027175492a1d3ba2
Juliano Alves <julianoalves@gmail.com>
2021-07-06 23:49:22 +0100

    End of select 11

--- Source/DatingApp/API/Controllers/AccountController.cs
+++ Source/DatingApp/API/Controllers/AccountController.cs
@@ -58,7 +58,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null || user.PasswordHash == null || user.PasswordSalt == null) 
                 return Unauthorized("Invalid username");
@@ -79,7 +81,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- Source/DatingApp/client/src/app/_services/account.service.ts
+++ Source/DatingApp/client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((response) => {
         const user = response;
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -31,14 +30,14 @@ register(model: any){
   return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
     map((user) => {
       if(user){
-        localStorage.setItem('user', JSON.stringify(user));
-        this.currentUserSource.next(user);
+        this.setCurrentUser(user);
       }
     })
   );
 }
 
   setCurrentUser(user: User){
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
