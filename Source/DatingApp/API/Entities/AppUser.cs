bdb038d6a2926073b94f3cbe2f60b5c393041170
Juliano Alves <julianoalves@gmail.com>
2021-07-08 09:05:49 +0100

    End section 13

--- Source/DatingApp/API/Entities/AppUser.cs
+++ Source/DatingApp/API/Entities/AppUser.cs
@@ -14,7 +14,7 @@ namespace API.Entities
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Create { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
---
+++ Source/DatingApp/API/Migrations/20210707144849_CorrectUserCreatedProp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class CorrectUserCreatedProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Create",
+                table: "Users",
+                newName: "Created");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Users",
+                newName: "Create");
+        }
+    }
+}
